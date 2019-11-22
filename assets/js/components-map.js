e55aa86ec207509998e31b418ab8cb0d1cfcc858
Ubuntu <ubuntu@ip-172-31-86-238.ec2.internal>
2019-11-22 00:03:13 +0000

    Adding new images, fixing layout, adding shadows

--- assets/js/components-map.js
+++ assets/js/components-map.js
@@ -1,30 +1,22 @@
-/*$(document).ready(function() {
-    //Toggles the selected class for each place type------------------------------------------
+$(document).ready(function() {
+    $("#submitButton").click(function() {
+        $("#myModal").modal("show");
+    });
+    $(".img-packages").mouseout(function() {
+      $(this).next().hide();
+    });
+    $(".img-packages").mouseover(function() {
+        $(this).next().show();
+    });
+    
+    
+     $(".card").mouseout(function() {
+      $(this).css("box-shadow", "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)");
+    });
+    $(".card").mouseover(function() {
+        $(this).css("box-shadow", "10px 10px 5px grey");
+    });
 
-    $('#accommodation').click(function() {
-        $('#restaurant').removeClass('active');
-        $('#attraction').removeClass('active');
-        $('#store').removeClass('active');
-        $('#accommodation').addClass('active');
-    });
-    $('#restaurant').click(function() {
-        $('#accommodation').removeClass('active');
-        $('#attraction').removeClass('active');
-        $('#store').removeClass('active');
-        $('#restaurant').addClass('active');
-    });
-    $('#attraction').click(function() {
-        $('#restaurant').removeClass('active');
-        $('#accommodation').removeClass('active');
-        $('#store').removeClass('active');
-        $('#attraction').addClass('active');
-    });
-    $('#store').click(function() {
-        $('#restaurant').removeClass('active');
-        $('#attraction').removeClass('active');
-        $('#accommodation').removeClass('active');
-        $('#store').addClass('active');
-    });
 
 });
-*/
+
--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,17 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "holiday_planner", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "booking_travel_query": contactForm.bookingtravelquery.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "booking_travel_query": contactForm.bookingtravelquery.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
 }
