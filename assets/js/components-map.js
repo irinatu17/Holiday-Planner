fa15377db9838b62e3d1a76ea9847c012d2e730e
Ubuntu <ubuntu@ip-172-31-86-238.ec2.internal>
2019-11-23 02:46:13 +0000

    Uploading pics, adding carousel to travel packages, alts, fixing responsivness of img

--- assets/js/components-map.js
+++ assets/js/components-map.js
@@ -1,22 +1,64 @@
 $(document).ready(function() {
-    $("#submitButton").click(function() {
-        $("#myModal").modal("show");
+
+    //Hides dropdown menus and popups when the page loads---------------------------------
+    $('.readmore-info').hide();
+    $('.img-carousel').hide();
+
+        $(".img-packages").mouseout(function() {
+            $(this).next().hide();
+        });
+
+
+        $(".img-packages").mouseover(function() {
+            $(this).next().show();
+        });
+
+    //Showing carousel modals by clicking the image--------------------------------------
+
+    $("#Morocco").click(function() {
+        $("#MoroccoModal").modal("show");
     });
-    $(".img-packages").mouseout(function() {
-      $(this).next().hide();
+    $("#MoroccoMountain").click(function() {
+        $("#MoroccoMountainsModal").modal("show");
     });
-    $(".img-packages").mouseover(function() {
-        $(this).next().show();
+    $("#Sicily").click(function() {
+        $("#SicilyModal").modal("show");
     });
-    
-    
-     $(".card").mouseout(function() {
-      $(this).css("box-shadow", "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)");
+    $("#russianNorth").click(function() {
+        $("#RussiaNorthModal").modal("show");
+    });
+    $("#russiaTranssiberian").click(function() {
+        $("#russiaTranssiberianNorthModal").modal("show");
+    });
+    $("#russiaUrals").click(function() {
+        $("#russiaUralsModal").modal("show");
+    });
+    $("#IrelandDrinks").click(function() {
+        $("#IrelandDrinksModal").modal("show");
+    });
+    $("#IrelandOcean").click(function() {
+        $("#IrelandOceanModal").modal("show");
+    });
+    $("#Greece").click(function() {
+        $("#GreeceModal").modal("show");
+    });
+
+
+
+
+
+
+    $(".card").mouseout(function() {
+        $(this).css("box-shadow", "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)");
     });
     $(".card").mouseover(function() {
         $(this).css("box-shadow", "10px 10px 5px grey");
     });
 
+    $(".btn-readmore").click(function() {
+        $(this).next("p.readmore-info").slideToggle();
+    });
+
+
 
 });
-
