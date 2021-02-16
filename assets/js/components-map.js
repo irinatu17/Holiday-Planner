f6f6142cc91c3f67f14d210e87feffeffb4cd1a1
irinatu17 <ira_tushina@mail.ru>
2021-02-16 12:41:07 +0000

    Back to top button added

--- assets/js/components-map.js
+++ assets/js/components-map.js
@@ -80,4 +80,27 @@ $(document).ready(function() {
         $('nav').toggleClass('scrolled', $(this).scrollTop() > 1080);
      });
 
+    /**
+     *  Back to Top button
+     */
+
+    $('.btt-link').click(function(e) {
+        window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+        })
+    })
+
 });
+
+bttbutton = document.getElementById("back-to-top-btn");
+// When the user scrolls down 50px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      bttbutton.style.display = "block";
+    } else {
+      bttbutton.style.display = "none";
+    }
+  }
