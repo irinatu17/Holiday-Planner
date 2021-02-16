c2c1c334c51d90fb19b10655fba055cd65e086a7
irinatu17 <ira_tushina@mail.ru>
2021-02-16 13:08:48 +0000

    Extra spaces added in the price part of the card

--- assets/js/components-map.js
+++ assets/js/components-map.js
@@ -57,15 +57,6 @@ $(document).ready(function() {
         $("#GreeceModal").modal("show");
     });
 
-    //Adding shadow to the cards when they are hovered
-    $(".card-img").mouseout(function() {
-        $(this).css("box-shadow", "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)");
-    });
-
-    $(".card-img").mouseover(function() {
-        $(this).css("box-shadow", "10px 10px 5px grey");
-    });
-
     //Stiding down and up the paragraph, when the Read more button is clicked
     // Update the text on the button to "Read less", when the paragraph is toggled
     $(".btn-readmore").click(function() {
@@ -80,27 +71,24 @@ $(document).ready(function() {
         $('nav').toggleClass('scrolled', $(this).scrollTop() > 1080);
      });
 
-    /**
-     *  Back to Top button
-     */
-
+    //Back to Top button
     $('.btt-link').click(function(e) {
         window.scrollTo({
         top: 0,
         left: 0,
         behavior: 'smooth'
         })
-    })
+    });
 
 });
 
 bttbutton = document.getElementById("back-to-top-btn");
 // When the user scrolls down 50px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction() {
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      bttbutton.style.display = "block";
+        bttbutton.style.display = "block";
     } else {
-      bttbutton.style.display = "none";
+        bttbutton.style.display = "none";
     }
-  }
+};
